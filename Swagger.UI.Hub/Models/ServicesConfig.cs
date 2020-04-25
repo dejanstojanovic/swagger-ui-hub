ef5642abc695a3a57b734e3203f6646a7a7c60ca
Dejan Stojanovic <dejan@evowition.com>
2020-04-25 22:29:05 +0400

    feat: initial commit

---
+++ Swagger.UI.Hub/Models/ServicesConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Swagger.UI.Hub.Models
+{
+    public class ServicesConfig
+    {
+        public IEnumerable<ServicesConfigItem> Services { get; set; }
+    }
+}
---
+++ Swagger.UI.Hub/Models/ServicesConfigItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Threading.Tasks;
+
+namespace Swagger.UI.Hub.Models
+{
+    public class ServicesConfigItem
+    {
+        public String Name { get; set; }
+        public Uri Url { get; set; }
+        public Boolean Default { get; set; }
+    }
+}
