1c9a8248a79b6f2466d35bc4e41e189ca6e38db7
Dejan Stojanovic <d.stojanovic@marcura.com>
2020-04-26 21:23:31 +0400

    feat: adds fluent validation for configuration options

---
+++ Swagger.UI.Hub/Validations/ServicesConfigValidation.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.Options;
+using Swagger.UI.Hub.Models;
+using System.Linq;
+
+
+namespace Swagger.UI.Hub.Validations
+{
+    public class ServicesConfigValidation : AbstractValidator<ServicesConfig>
+    {
+        public ServicesConfigValidation()
+        {
+            RuleFor(m => m).NotNull().WithMessage("No services configuration present");
+            RuleFor(m => m.Services).NotNull().NotEmpty().WithMessage("No services defined");
+            When(m => m.Services != null, () =>
+               {
+                   RuleFor(m => m.Services).Must(s => s.GroupBy(s => s.Name).Count() == s.Count()).WithMessage("Service configuration names must be unique");
+                   RuleFor(m => m.Services).Must(s => s.Count(s => s.Default) <= 1).WithMessage("Only one service can be configured as default");
+               });
+        }
+    }
+}
